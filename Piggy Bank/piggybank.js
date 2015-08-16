5980ba28691bd07249f5704efebd3ca7a6807434
Eric Pollard <ericpoll@users.noreply.github.com>
2015-08-16 19:02:43 -0400

    drag the quarter into the piggybank!

---
+++ Piggy Bank/piggybank.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var qposition=$("#quarter").position();
+  $("#quarter").draggable({
+    containment: "body"});
+
+  $("#piggybank").droppable({
+    drop: function () {
+      $("#quarter").css("left",qposition.left); //reset position of quarter
+      $("#quarter").css("top",qposition.top);
+      $("#sample")[0].play(); //play sample when dropped over piggybank
+    }
+  });
+});
