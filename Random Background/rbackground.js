a8333103637730bfbcf91576ef204d011e59e300
Eric Pollard <ericpoll@users.noreply.github.com>
2015-09-14 02:08:35 -0400

    added minor fixes related to cursor

--- Random Background/rbackground.js
+++ Random Background/rbackground.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+
+  $("#randomcolor").hover(function() {
+    $("#randomcolor").css("cursor","pointer");
+  });
+
   $("#randomcolor").click(function () {
     var ranhexnum = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     $("body").css("background-color",ranhexnum);
